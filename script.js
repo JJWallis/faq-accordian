17766851e641c0b8d055640e4c3329564da7446d
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-06-23 15:18:58 +0100

    Unfinished - Arrow Rotation + Number/Sizing

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 'use strict'
 
-const links = document.querySelectorAll('a');
+const links = document.getElementsByTagName('a');
 const article = document.querySelector('article');
-const wrapper = document.querySelector('.wrapper');
 
-
-function appendArrowButtons () {
+function appendArrowButtons() {
     const arrow = document.createElement('img');
     arrow.src = '/images/icon-arrow-down.svg';
     arrow.alt = 'arrow-image';
-    arrow.className = 'arrow'
-    arrow.style.marginLeft = '70px';
+    arrow.className = 'arrow';
 
     for (let i = 0; i < links.length; i++) {
         links[i].appendChild(arrow);
@@ -19,9 +16,20 @@ function appendArrowButtons () {
 
 appendArrowButtons();
 
-
 article.addEventListener('click', (e) => {
     if (e.target.tagName === 'A') {
-        // e.target.style.fontWeight = 'bold';
+        const link = e.target;
+        const nextSibling = link.nextElementSibling;
+        link.classList.toggle('bold');
+        nextSibling.classList.toggle('hidden');
+        link.classList.toggle('seperator');
+        nextSibling.classList.toggle('seperator');
+        if (!nextSibling.classList.contains('hidden')) {
+            link.style.paddingBottom = '10px';
+        } else {
+            link.style.paddingBottom = '20px';
+        }
     }
 });
+
+// Invert arrow icon
