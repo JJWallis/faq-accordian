51baf12c373dfe100455c1c32f7d97b3f9554461
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-12 15:43:59 +0000

    CSS refactored + arrows rotation fixe

--- script.js
+++ script.js
@@ -3,25 +3,12 @@
 const links = document.getElementsByTagName('a')
 const article = document.querySelector('article')
 
-function appendArrowIcons() {
-   for (let i = 0; i < links.length; i++) {
-      const arrow = document.createElement('img')
-      arrow.src = '/images/icon-arrow-down.svg'
-      arrow.alt = 'arrow-image'
-      arrow.className = 'arrow'
-      links[i].appendChild(arrow)
-   }
-}
-
-appendArrowIcons()
-
 article.addEventListener('click', (e) => {
    if (e.target.tagName === 'A') {
       const link = e.target
-      const linkId = link.getAttribute('id')
       const nextSibling = link.nextElementSibling
-      const arrow = document.querySelector(`#${linkId} img`)
       link.classList.toggle('bold')
+      link.classList.toggle('active')
       nextSibling.classList.toggle('hidden')
       link.classList.toggle('seperator')
       nextSibling.classList.toggle('seperator')
@@ -29,11 +16,6 @@ article.addEventListener('click', (e) => {
          link.style.paddingBottom = '10px'
       } else {
          link.style.paddingBottom = '20px'
-      }
-      if (!arrow.classList.contains('invert')) {
-         arrow.classList.add('invert')
-      } else {
-         arrow.classList.remove('invert')
       }
    }
 })
